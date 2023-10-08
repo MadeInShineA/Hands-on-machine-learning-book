7f9e8da46376343ccdcbc41c72d030b57054a9b4
MadeInShineA <olivier.amacker@netplus.ch>
2023-10-08 23:17:33 +0200

    Split data between train and test set + began data exploration

--- california-housing-price/main.py
+++ california-housing-price/main.py
@@ -5,6 +5,7 @@ import urllib.request
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def load_housing_data():
@@ -26,34 +27,66 @@ def shuffle_and_split_data(data, test_ratio):
     return data.iloc[train_indices], data.iloc[test_indices]
 
 
+def stratified_split_test_train(n_splits, test_size, data, stratified_by):
+    splitter = StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size, random_state=42)
+    stratified_splits = []
+    for train_index, test_index in splitter.split(data, data[stratified_by]):
+        stratified_splits.append((data.loc[train_index], data.loc[test_index]))
+    return stratified_splits
+
+
 if __name__ == '__main__':
     housing_data = load_housing_data()
 
-    print("Dataset Informations\n")
+    print("*** Dataset Informations ***\n")
     print(housing_data.info())
 
-    print("\n**************\n")
+    print("\n*** Dataset Head***\n")
 
-    print("Dataset Head\n")
     print(housing_data.head())
 
-    print("\n**************\n")
+    print("\n*** Ocean Proximity Value Counts ***\n")
 
-    print("Ocean Proximity Value Counts\n")
     print(housing_data["ocean_proximity"].value_counts())
 
-    print("\n**************\n")
+    print("\n*** Dataset Description ***\n")
 
-    print("Dataset Description\n")
     print(housing_data.describe())
 
-    print("\n**************\n")
+    print("\n*** Dataset Histogram ***\n")
 
-    print("Dataset Histogram\n")
     housing_data.hist(bins=50, figsize=(20, 15))
     plt.show()
 
+    # Creating the income_cat attribute to allow stratified sampling over the different incomes
+    housing_data["income_cat"] = pd.cut(housing_data["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
+    housing_data["income_cat"].hist()
+    plt.show()
+
+    # Splitting the data randomly
+    train_set, test_set = train_test_split(housing_data, test_size=0.2, random_state=42)
+
+    # Splitting the data using stratified sampling with the income categories we created
+    stratified_train_set, stratified_test_set = stratified_split_test_train(1, 0.2, housing_data, "income_cat")[0]
+
+    # Removing the income_cat attribute from the data
+    for set_ in (stratified_train_set, stratified_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
 
 
+    original_stratified_train_set = stratified_train_set.copy()
 
+    # Visualizing the data with the longitude and latitude attributes
+    housing_data.plot(kind="scatter", x="longitude", y="latitude", alpha=0.2,)
+    plt.show()
 
+    # Visualizing the data with the longitude and latitude attributes and the housing prices
+    housing_data.plot(kind="scatter", x="longitude", y="latitude", grid=True, s=housing_data["population"]/100,
+                      label="population", figsize=(10, 7), c="median_house_value", cmap=plt.get_cmap("jet"),
+                      colorbar=True, legend=True, sharex=False)
+    plt.show()
+
+    # Looking for correlations
+    print("*** Looking for correlations ***\n")
+    correlation_matrix = housing_data.corr()
+    print(correlation_matrix["median_house_value"].sort_values(ascending=False))
